3aa05b470e8f47d0d92aab99cd5e50bc358c42a8
Kongwen Shuo <2806144405@qq.com>
2025-03-03 23:14:11 +0800

    添加聊天服务器

---
+++ Chat_Server/AsioIOServicePool.cpp
@@ -0,0 +1,49 @@
+﻿#include "AsioIOServicePool.h"
+#include <iostream>
+using namespace std;
+AsioIOServicePool::AsioIOServicePool(std::size_t size):_ioServices(size),
+_works(size), _nextIOService(0){
+	for (std::size_t i = 0; i < size; ++i) {
+		_works[i] = std::make_unique<Work>(boost::asio::make_work_guard(_ioServices[i]));
+		/*
+		_works【i】 = std::unique_ptr<Work>(new Work(_ioServices【i】.get_executor()));
+		*/
+	}
+	// 遍历多个 io_context，创建多个线程，每个线程内部启动 io_context
+	for (std::size_t i = 0; i < _ioServices.size(); ++i) {
+		_threads.emplace_back([this, i]() {
+			_ioServices[i].run();
+			});
+	}
+}
+
+AsioIOServicePool::~AsioIOServicePool() {
+	std::cout << "AsioIOServicePool destruct" << endl;
+}
+
+boost::asio::io_context& AsioIOServicePool::GetIOService() {
+	auto& service = _ioServices[_nextIOService++];
+	if (_nextIOService == _ioServices.size()) {
+		_nextIOService = 0;
+	}
+	return service;
+}
+
+void AsioIOServicePool::Stop(){
+	// 停止所有 io_context 服务
+	for (auto& ioService : _ioServices) {
+		ioService.stop();
+	}
+
+	// 释放所有 work_guard，允许 io_context 在没有任务时退出
+	for (auto& work : _works) {
+		work.reset();
+	}
+
+	// 等待所有线程结束
+	for (auto& thread : _threads) {
+		if (thread.joinable()) {
+			thread.join();
+		}
+	}
+}
