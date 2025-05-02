4d7de04e2527fa3346a6249255a81184313a1e70
Kongwen Shuo <2806144405@qq.com>
2025-05-03 00:10:52 +0800

    修改状态服务器动态分配逻辑,并且添加分布式锁

---
+++ StatusServer/CServer.cpp
@@ -0,0 +1,47 @@
+#include "CServer.h"
+#include <iostream>
+#include "AsioIOServicePool.h"
+#include "UserMgr.h"
+CServer::CServer(boost::asio::io_context& io_context, short port):_io_context(io_context), _port(port),
+_acceptor(io_context, tcp::endpoint(tcp::v4(),port))
+{
+	cout << "Server start success, listen on port : " << _port << endl;
+	StartAccept();
+}
+
+CServer::~CServer() {
+	cout << "Server destruct listen on port : " << _port << endl;
+}
+
+void CServer::HandleAccept(shared_ptr<CSession> new_session, const boost::system::error_code& error){
+	if (!error) {
+		new_session->Start();
+		lock_guard<mutex> lock(_mutex);
+		_sessions.insert(make_pair(new_session->GetSessionId(), new_session));
+	}
+	else {
+		cout << "session accept failed, error is " << error.what() << endl;
+	}
+
+	StartAccept();
+}
+
+void CServer::StartAccept() {
+	auto &io_context = AsioIOServicePool::GetInstance()->GetIOService();
+	shared_ptr<CSession> new_session = make_shared<CSession>(io_context, this);
+	_acceptor.async_accept(new_session->GetSocket(), std::bind(&CServer::HandleAccept, this, new_session, placeholders::_1));
+}
+
+void CServer::ClearSession(std::string session_id) {
+	//lock_guard<mutex> lock(_mutex);
+	std::cout << "session_id------------------" << session_id << std::endl;
+	std::cout << "_session+++++++++++++" << _sessions.empty() << std::endl;
+	if (_sessions.find(session_id) != _sessions.end()) {
+		auto uid = _sessions[session_id]->GetUserId();
+
+		//移除用户和session的关联
+		UserMgr::GetInstance()->RmvUserSession(uid, session_id);
+	}
+	_sessions.erase(session_id);
+	
+}
